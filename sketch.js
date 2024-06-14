d76a252be64851f8588fb1819284d4dc146a25d6
Yashmita Sharma <71992108+xyashmitax@users.noreply.github.com>
2024-06-14 13:17:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -136,7 +136,7 @@ function spawnClouds() {
     cloud.velocityX = -3;
     
      //assign lifetime to the variable
-    cloud.lifetime = 600;
+    cloud.lifetime = 10500;
     
     //adjust the depth
     cloud.depth = trex.depth;
